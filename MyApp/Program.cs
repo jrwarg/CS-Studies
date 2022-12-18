fed10f48e3f1bf7b3ab1d1d92841b08c8d438a6b
JR Guerra <jrwarg@gmail.com>
2022-12-18 16:53:28 -0300

    First lessons

---
+++ MyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var text = "Testing";
+            Console.WriteLine(text);
+        }
+    }
+}
